e8ad4705c1bd5b4fefdc82aac864c8164475d58a
MateoBitar <bitarmateo21@gmail.com>
2025-03-26 17:05:47 +0200

    Update

--- ZVote/src/main/java/com/example/zvote/Models/CandidateModel.java
+++ ZVote/src/main/java/com/example/zvote/Models/CandidateModel.java
@@ -1,4 +1,79 @@
 package com.example.zvote.Models;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class CandidateModel {
+    private int candidate_ID;
+    private String name;
+    private byte[] photo;
+    private String bio;
+    private int poll_ID;
+
+    public CandidateModel(String name, byte[] photo, String bio, int poll_ID) {
+        setName(name);
+        setPhoto(photo);
+        setBio(bio);
+        setPoll_ID(poll_ID);
+    }
+
+    public int getCandidate_ID() {
+        return candidate_ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        if(photo != null){
+            this.photo = photo;
+        }
+        else {
+            try {
+                // Use a default photo path and convert it to a byte array
+                Path defaultPhotoPath = Path.of("\uD83D\uDC64");
+                this.photo = Files.readAllBytes(defaultPhotoPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+                this.photo = new byte[0]; // Fallback to empty byte array on error
+            }
+        }
+
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public int getPoll_ID() {
+        return poll_ID;
+    }
+
+    public void setPoll_ID(int poll_ID) {
+        this.poll_ID = poll_ID;
+    }
+
+    @Override
+    public String toString() {
+        return "candidate_Id = " + candidate_ID +
+                ", name = " + name +
+                ", photo = " + Arrays.toString(photo) +
+                ", bio = " + bio +
+                ", poll_ID = " + poll_ID;
+    }
 }
--- ZVote/src/main/java/com/example/zvote/Models/VoteModel.java
+++ ZVote/src/main/java/com/example/zvote/Models/VoteModel.java
@@ -1,4 +1,63 @@
 package com.example.zvote.Models;
 
+import java.time.LocalDateTime;
+
 public class VoteModel {
+    private int user_ID;
+    private int poll_ID;
+    private LocalDateTime timestamp;
+    private int blank;
+    private int candidate_ID;
+
+    public VoteModel(int user_ID, int poll_ID, int blank, int candidate_ID) {
+        setUser_ID(user_ID);
+        setPoll_ID(poll_ID);
+        setBlank(blank);
+        setCandidate_ID(candidate_ID);
+    }
+
+    public int getUser_ID() {
+        return user_ID;
+    }
+
+    public void setUser_ID(int user_ID) {
+        this.user_ID = user_ID;
+    }
+
+    public int getPoll_ID() {
+        return poll_ID;
+    }
+
+    public void setPoll_ID(int poll_ID) {
+        this.poll_ID = poll_ID;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getBlank() {
+        return blank;
+    }
+
+    public void setBlank(int blank) {
+        this.blank = blank;
+    }
+
+    public int getCandidate_ID() {
+        return candidate_ID;
+    }
+
+    public void setCandidate_ID(int candidate_ID) {
+        this.candidate_ID = candidate_ID;
+    }
+
+    @Override
+    public String toString() {
+        return "user_ID = " + user_ID +
+                ", poll_ID = " + poll_ID +
+                ", timestamp = " + timestamp +
+                ", blank = " + blank +
+                ", candidate_ID = " + candidate_ID;
+    }
 }
