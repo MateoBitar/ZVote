1415d70eea69c1024202ccee707a591acdb19225
Marco <bitar.marco21@gmail.com>
2025-03-26 16:13:42 +0200

    Update

--- ZVote/src/main/java/com/example/zvote/Models/PollModel.java
+++ ZVote/src/main/java/com/example/zvote/Models/PollModel.java
@@ -1,4 +1,93 @@
 package com.example.zvote.Models;
 
+import java.util.Date;
+
 public class PollModel {
+    private int poll_ID;
+    private String title;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    public enum Status {
+        INACTIVE,
+        ACTIVE,
+        COMPLETED
+    }
+    private Status status;
+    private int nbOfVotes;
+    private int nbOfAbstentions;
+    private int admin_ID;
+
+    public PollModel(String title, String description, Date start_date, Date end_date) {
+        this.title = title;
+        this.description = description;
+        this.start_date = start_date;
+        this.end_date = end_date;
+    }
+
+    public int getPoll_ID() {
+        return poll_ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public int getNbOfVotes() {
+        return nbOfVotes;
+    }
+
+    public int getNbOfAbstentions() {
+        return nbOfAbstentions;
+    }
+
+    public int getAdmin_ID() {
+        return admin_ID;
+    }
+
+    @Override
+    public String toString() {
+        return "poll_ID=" + poll_ID +
+                ", title = '" + title + '\'' +
+                ", description = '" + description + '\'' +
+                ", start_date = " + start_date +
+                ", end_date = " + end_date +
+                ", status = " + status +
+                ", nbOfVotes = " + nbOfVotes +
+                ", nbOfAbstentions = " + nbOfAbstentions +
+                ", admin_ID = " + admin_ID;
+    }
 }
--- ZVote/src/main/java/com/example/zvote/Models/UserModel.java
+++ ZVote/src/main/java/com/example/zvote/Models/UserModel.java
@@ -1,4 +1,110 @@
 package com.example.zvote.Models;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class UserModel {
+    private int user_ID;
+    private String username;
+    private String user_email;
+    private String user_pass;
+    private byte[] user_photoID;
+    private String phoneNb;
+    private String role;
+
+    public UserModel(String username, String user_email, String user_pass, byte[] user_photoID, String phoneNb) {
+        setUsername(username);
+        setUser_email(user_email);
+        setUser_pass(user_pass);
+        setUser_photoID(user_photoID);
+        setPhoneNb(phoneNb);
+        role = "USER";
+    }
+
+    public int getUser_ID() {
+        return user_ID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public String getUser_pass() {
+        return user_pass;
+    }
+
+    public void setUser_pass(String user_pass) {
+        this.user_pass = hashPassword(user_pass);
+    }
+
+    public byte[] getUser_photoID() {
+        return user_photoID;
+    }
+
+    public void setUser_photoID(byte[] user_photoID) {
+        if(user_photoID != null){
+            this.user_photoID = user_photoID;
+        }
+        else {
+            try {
+                // Use a default photo path and convert it to a byte array
+                Path defaultPhotoPath = Path.of("\uD83D\uDC64");
+                this.user_photoID = Files.readAllBytes(defaultPhotoPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+                this.user_photoID = new byte[0]; // Fallback to empty byte array on error
+            }
+        }
+
+    }
+
+    public String getPhoneNb() {
+        return phoneNb;
+    }
+
+    public void setPhoneNb(String phoneNb) {
+        this.phoneNb = phoneNb;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    private String hashPassword(String password) {
+        return stringToHex(password);
+    }
+
+    public String stringToHex(String input) {
+        byte[] bytes = input.getBytes(); // Convert String to byte array
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            hexString.append(String.format("%02x", b)); // Format each byte as a hex value
+        }
+        return hexString.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "user_ID = " + user_ID +
+                ", username = '" + username + '\'' +
+                ", user_email = '" + user_email + '\'' +
+                ", user_pass = '" + user_pass + '\'' +
+                ", user_photoID = " + Arrays.toString(user_photoID) +
+                ", phoneNb = '" + phoneNb + '\'' +
+                ", role = '" + role + '\'';
+    }
 }
